b9d3449cf71b0b2869dd08c1838b5672fcf6dcce
iamsumitsk <sk.sumit0852@gmail.com>
2019-10-18 19:58:58 +0530

    added js file with answers

---
+++ app.js
@@ -0,0 +1,32 @@
+const correctAnswers = ['A','D','C','A','D','C','A','A','D','B'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  let score = 0;
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, form.q9.value, form.q10.value];
+
+  // check the answers
+  userAnswers.forEach((answer, index) => {
+    if (answer === correctAnswers[index]){
+      score += 10;
+    }
+  });
+
+  // show the result
+  scrollTo(0, 0);
+  result.classList.remove('d-none');
+
+  let output = 0;
+  const timer = setInterval(() => {
+    result.querySelector('span').textContent = `${output}%`;
+    if(output === score){
+      clearInterval(timer);
+    } else {
+      output++;
+    }
+  }, 10);
+
+});
